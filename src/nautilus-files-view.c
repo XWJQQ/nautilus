a571f01936da494a3c1a2852b36552c516ddca95
António Fernandes <antoniof@gnome.org>
2022-04-03 00:29:52 +0100

    tag-manager: Stop getting full references
    It's really annoying having to setup a local autocleanup variable in
    every function we want to use the NautilusTagManager API.
    
    Use a NautilusUndoManager-like API instead, with new() returning a
    reference and get() not.

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -273,7 +273,6 @@ typedef struct
     GCancellable *clipboard_cancellable;
 
     GCancellable *starred_cancellable;
-    NautilusTagManager *tag_manager;
 
     gulong name_accepted_handler_id;
     gulong cancelled_handler_id;
@@ -1644,7 +1643,7 @@ action_star (GSimpleAction *action,
     priv = nautilus_files_view_get_instance_private (view);
     selection = nautilus_view_get_selection (NAUTILUS_VIEW (view));
 
-    nautilus_tag_manager_star_files (priv->tag_manager,
+    nautilus_tag_manager_star_files (nautilus_tag_manager_get (),
                                      G_OBJECT (view),
                                      selection,
                                      NULL,
@@ -1664,7 +1663,7 @@ action_unstar (GSimpleAction *action,
     priv = nautilus_files_view_get_instance_private (view);
     selection = nautilus_view_get_selection (NAUTILUS_VIEW (view));
 
-    nautilus_tag_manager_unstar_files (priv->tag_manager,
+    nautilus_tag_manager_unstar_files (nautilus_tag_manager_get (),
                                        G_OBJECT (view),
                                        selection,
                                        NULL,
@@ -3343,7 +3342,6 @@ nautilus_files_view_finalize (GObject *object)
 
     g_cancellable_cancel (priv->starred_cancellable);
     g_clear_object (&priv->starred_cancellable);
-    g_clear_object (&priv->tag_manager);
 
     G_OBJECT_CLASS (nautilus_files_view_parent_class)->finalize (object);
 }
@@ -7797,7 +7795,7 @@ real_update_actions_state (NautilusFilesView *view)
 
     current_location = nautilus_file_get_location (nautilus_files_view_get_directory_as_file (view));
     current_uri = g_file_get_uri (current_location);
-    can_star_current_directory = nautilus_tag_manager_can_star_contents (priv->tag_manager, current_location);
+    can_star_current_directory = nautilus_tag_manager_can_star_contents (nautilus_tag_manager_get (), current_location);
 
     show_star = (selection != NULL) &&
                 (can_star_current_directory || selection_contains_starred);
@@ -7815,7 +7813,7 @@ real_update_actions_state (NautilusFilesView *view)
             break;
         }
 
-        if (nautilus_tag_manager_file_is_starred (priv->tag_manager, uri))
+        if (nautilus_tag_manager_file_is_starred (nautilus_tag_manager_get (), uri))
         {
             show_star = FALSE;
         }
@@ -9708,7 +9706,6 @@ nautilus_files_view_init (NautilusFilesView *view)
     nautilus_application_set_accelerators (app, "view.popup-menu", popup_menu_accels);
 
     priv->starred_cancellable = g_cancellable_new ();
-    priv->tag_manager = nautilus_tag_manager_get ();
 
     priv->rename_file_controller = nautilus_rename_file_popover_controller_new ();
 
